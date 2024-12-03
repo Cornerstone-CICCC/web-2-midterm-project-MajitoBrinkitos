e089ac23ba9de18acf1aa7a88ca2d9ba2a8bdec4
Majito Brinkitos <mcvef2013@gmail.com>
2024-12-03 11:58:42 -0800

    files 2024-12-03

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# CINEPHILE - Your ticket to great entertainment 
+
+
+
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/0S8pm_d1)
 # Web 2 - Midterm: Movie Web App
 
--- app.js
+++ app.js
@@ -10,65 +10,86 @@
      alert("Support Team Link");
  }
 
+//consts
+const displayResults = document.querySelector('.results');
+
  //API
  const apiReadAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkM2IyMWVlOTlmNzMyYjFiMzU5NTE4NzM3MDJmNDU4MiIsIm5iZiI6MTczMzE2MzYxNC4zNzIsInN1YiI6IjY3NGRmYTVlNTYyYjAzMGJiNWFkZTVhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SN9C63wqn3Bqd9cEYM8R3nOHOsZMpyMipzF4snctfLs';
 
  //Event listeners for the Search Button
  document.getElementById('searchBtn').addEventListener('click', function() {
     const searchMedia = document.getElementById('searchMedia').value;
+
+    //
+    if (!searchMedia) {
+        alert('Please enter a search term');
+        return;
+    }
+
     const movieUrl = `https://api.themoviedb.org/3/search/movie?query=${searchMedia}`;
     const tvUrl = `https://api.themoviedb.org/3/search/tv?query=${searchMedia}`;
 
-//Requesting Access using the Read Access Token: MOVIES
-const fetchMovieData = fetch(movieUrl, {
-    method: 'GET',
-    headers: {
-        'Authorization': `Bearer ${apiReadAccessToken}`
-    }
-});
+        //Requesting Access using the Read Access Token: MOVIES
+        const fetchMovieData = fetch(movieUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${apiReadAccessToken}`
+            }
+        });
 
-//Requesting Access using the Read Access Token: TV SERIES
-const fetchTvData = fetch(tvUrl, {
-    method: 'GET',
-    headers:{
-        'Authorization': `Bearer ${apiReadAccessToken}`
-    }
-});
+        //Requesting Access using the Read Access Token: TV SERIES
+        const fetchTvData = fetch(tvUrl, {
+            method: 'GET',
+            headers:{
+                'Authorization': `Bearer ${apiReadAccessToken}`
+            }
+        });
     
 //Responses
    Promise.all([fetchMovieData, fetchTvData])
    .then(responses => Promise.all(responses.map(response => response.json())))
    .then(([movieData, tvData]) => {
-        const resultsContainer = document.getElementById('results');
+        const resultsContainer = document.querySelector('.results');
         resultsContainer.innerHTML = ''; //to clear previous results
 
         //movies results
-        movieData.results.forEach(movie => {
-            const div = document.createElement('div');
-            const imageUrl = `https://image.tmdb.org/t/p/w200${movie.poster_path}`;
-            div.innerHTML = `
-            <h2>${movie.title}</h2>
-            <p>Release Date: ${movie.release_date}</p>
-            <img src="${imageUrl}" alt="${movie.title}" width="200">
-            `;
-            resultsContainer.appendChild(div);
-        });
+        if(movieData.results.length > 0){
+            movieData.results.forEach(movie => {
+                const div = document.createElement('div');
+                const imageUrl = movie.poster_path ? `https://image.tmdb.org/t/p/w200${movie.poster_path}` : `placeholder.png`;
+                div.innerHTML = `
+                <div class="results">
+                    <img src="${imageUrl}" alt="${movie.title}" width="200">
+                    <h2>${movie.title}</h2>
+                    <p>Popularity Vote: ${movie.popularity}</p>
+                </div>            
+                `;
+                resultsContainer.appendChild(div);
+            });
+        } else {
+            resultsContainer.innerHTML += '<p>No movie results found.</p>';
+        }
 
         //tv series results
-        tvData.results.forEach(tvShow => {
-            const div = document.createElement('div');
-            const imageUrl = `https://image.tmdb.org/t/p/w200${tvShow.poster_path}`;
-            div.innerHTML =  `
-            <h2>${tvShow.name}</h2>
-            <p>First Air Date: ${tvShow.first_air_date}</p>
-            <img src="${imageUrl}" alt="${tvShow.name}" width="200">
-            `;
-            resultsContainer.appendChild(div);
-        });
+        if(tvData.results.length > 0){
+            tvData.results.forEach(tvShow => {
+                const div = document.createElement('div');
+                const imageUrl = tvShow.poster_path ? `https://image.tmdb.org/t/p/w200${tvShow.poster_path}` : `placeholder.png`;
+                div.innerHTML =  `
+                <div class="results">
+                    <img src="${imageUrl}" alt="${tvShow.name}" width="200">
+                    <h2>${tvShow.name}</h2>
+                </div>
+                `;
+                resultsContainer.appendChild(div);
+            });
+        } else {
+            resultsContainer.innerHTML += '<p>No tv series results found.</p>';
+        }
    })
    .catch(error => {
         console.error('Error:', error);
-        const resultsContainer = document.getElementById('results');
+        const resultsContainer = document.querySelector('.results');
         resultsContainer.innerHTML = '<p>An error has occurred. Try later.</p>'
    });
  });
