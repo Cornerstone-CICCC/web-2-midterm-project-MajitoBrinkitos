83f2854723717b7f5e3b71ebf8281c56a627c3cc
Majito Brinkitos <mcvef2013@gmail.com>
2024-12-06 23:34:47 -0800

    2024-12-06

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+ //dark-light mode
+ document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleBtn = document.getElementById('bd-theme');
+    const themeOptions = document.querySelectorAll('[data-bs-theme-value]');
+
+    //user's theme preference
+    const currentTheme = localStorage.getItem('theme');
+    if(currentTheme){
+        document.documentElement.setAttribute('data-bs-theme', currentTheme);
+        document.getElementById('bd-theme-text').innerText = `Toggle theme (${currentTheme})`;
+    }
+
+    //Theme toggle button
+    themeOptions.forEach(option => {
+        option.addEventListener('click', () => {
+            const theme = option.getAttribute('data-bs-theme-value');
+            document.documentElement.setAttribute('data-bs-theme', theme);
+            document.getElementById('bd-theme-text').innerText = `Toggle theme (${theme})`;
+            localStorage.setItem('theme', theme);
+
+            //update button
+            themeOptions.forEach(opt => opt.classList.remove('active'));
+            option.classList.add('active');
+        });
+    });
+ });
+ 
  //Sidebar Menu
  //overlay
  document.querySelector('.overlay').addEventListener('click', function() {
@@ -15,16 +42,6 @@ const displayResults = document.querySelector('.results');
 
   //API: database
   const apiReadAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkM2IyMWVlOTlmNzMyYjFiMzU5NTE4NzM3MDJmNDU4MiIsIm5iZiI6MTczMzE2MzYxNC4zNzIsInN1YiI6IjY3NGRmYTVlNTYyYjAzMGJiNWFkZTVhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SN9C63wqn3Bqd9cEYM8R3nOHOsZMpyMipzF4snctfLs';
-
- //modal
-/* document.querySelectorAll('.modal').forEach(modal => {
-    modal.addEventListener('shown.bs.modal', () => {
-        const iframe = modal.querySelector('.modal-body');
-        if(iframe) {
-            iframe.focus();
-        }
-    });
-}); */
 
 
  //API: video
